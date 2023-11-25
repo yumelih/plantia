717f64f7e485df0d481e9fbadd2ba1fef4341be7
yumelih <yumelih@gmail.com>
2023-11-25 15:35:15 +0300

    plantia home file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <p>war</p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/features/Home/HomeHeader.jsx
@@ -0,0 +1,5 @@
+function HomeHeader() {
+  return <div></div>;
+}
+
+export default HomeHeader;
